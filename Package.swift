0b85688078c263afe5853bb137b5c5c4c6966df9
Simon Whitty <simon@whileloop.com>
2021-02-20 15:57:55 +1100

    Swift Package Manager Support

--- Package.swift
+++ Package.swift
@@ -1,19 +1,24 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Bagel",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+    ],
     products: [
         .library(name: "Bagel", targets: ["Bagel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AccioSupport/CocoaAsyncSocket.git", .branch("master")),
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.0.0"),
     ],
     targets: [
         .target(
             name: "Bagel",
             dependencies: ["CocoaAsyncSocket"],
-            path: "iOS/Source"
+            path: "iOS/Source",
+			publicHeadersPath: ""
         )
     ]
 )
