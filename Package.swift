97d61099c6499da216c7bb43934c3222fa26672d
Paul-Anatole CLAUDOT <p.claudot@betclicgroup.com>
2021-06-08 21:40:06 +0200

    [Bagel] Fix for betclic spm

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "Bagel", targets: ["Bagel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.0.0"),
+        .package(url: "https://github.com/paulanatoleclaudot-betclic/CocoaAsyncSocket.git", .revision("9b2cfedabfa421523e5edd9f2409999ba567a427")),
     ],
     targets: [
         .target(
